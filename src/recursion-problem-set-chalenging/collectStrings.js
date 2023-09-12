0091b9df8b6633538f0c1e8016afb1116ca5c27b
Mindi Weik <mindi@sierrainteractive.com>
2023-09-12 09:33:07 -0600

    Recursion-Challenging: collectString

--- src/recursion-problem-set-chalenging/collectStrings.js
+++ src/recursion-problem-set-chalenging/collectStrings.js
@@ -1,5 +1,23 @@
 const collectStrings = (object) => {
-  
+  let result = []
+
+  const helper = (obj) => {
+    let keys = Object.keys(obj)
+    if (keys.length === 0) return
+
+    for (let i = 0; i < keys.length; i++) {
+      let current = obj[keys[i]]
+      if (typeof current === 'string') {
+        result.push(current)
+      } else if (typeof current === 'object' && !Array.isArray(current)) {
+        helper(current)
+      }
+    }
+  }
+
+  helper(object)
+
+  return result
 }
 
 const obj = {
@@ -18,4 +36,6 @@ const obj = {
   }
 }
 
-collectStrings(obj) // ["foo", "bar", "baz"])
+const test1 = collectStrings(obj) // ["foo", "bar", "baz"])
+
+console.log(test1)
