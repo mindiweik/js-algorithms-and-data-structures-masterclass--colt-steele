91c403233a82b768c8122b8af282079d990c06b1
Mindi Weik <mindi@sierrainteractive.com>
2023-09-12 09:28:59 -0600

    Recursion-Challenging: stringifyNumbers

---
+++ src/recursion-problem-set-chalenging/collectStrings.js
@@ -0,0 +1,21 @@
+const collectStrings = (object) => {
+  
+}
+
+const obj = {
+  stuff: "foo",
+  data: {
+      val: {
+          thing: {
+              info: "bar",
+              moreInfo: {
+                  evenMoreInfo: {
+                      weMadeIt: "baz"
+                  }
+              }
+          }
+      }
+  }
+}
+
+collectStrings(obj) // ["foo", "bar", "baz"])
